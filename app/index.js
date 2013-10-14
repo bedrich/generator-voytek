1a9a47a9d641df52d867480a1875fecc0c3fb278
(dealwithit) mysterion <me@bedrichrios.com>
2013-10-14 09:31:16 -0400

    Adds main.js template file. Cleans up indentation for Gruntfile and Index.js.

--- app/index.js
+++ app/index.js
@@ -3,56 +3,56 @@ var util = require('util');
 var path = require('path');
 var yeoman = require('yeoman-generator');
 
-
 var FkGenerator = module.exports = function FkGenerator(args, options, config) {
-  yeoman.generators.Base.apply(this, arguments);
+    yeoman.generators.Base.apply(this, arguments);
 
-  this.on('end', function () {
-    this.installDependencies();
-  });
+    this.on('end', function () {
+        this.installDependencies();
+    });
 
-  this.pkg = JSON.parse(this.readFileAsString(path.join(__dirname, '../package.json')));
+    this.pkg = JSON.parse(this.readFileAsString(path.join(__dirname, '../package.json')));
 };
 
 util.inherits(FkGenerator, yeoman.generators.Base);
 
 FkGenerator.prototype.askFor = function askFor() {
-  var cb = this.async();
+    var cb = this.async();
 
-  // have Yeoman greet the user.
-  console.log(this.yeoman);
+    // have Yeoman greet the user.
+    console.log(this.yeoman);
 
-  var prompts = [{
-    name: 'appName',
-    message: 'What do you want to call your app?',
-    default: 'chompytown'
-  }];
+    var prompts = [{
+        name: 'appName',
+        message: 'What do you want to call your app?',
+        default: 'chompytown'
+    }];
 
-  this.prompt(prompts, function (props) {
-    this.appName = props.appName;
+    this.prompt(prompts, function (props) {
+        this.appName = props.appName;
 
-    cb();
-  }.bind(this));
+        cb();
+    }.bind(this));
 };
 
 FkGenerator.prototype.app = function app() {
-  this.mkdir('scss');
-  this.mkdir('css');
-  this.mkdir('js');
-  this.mkdir('img');
+    this.mkdir('scss');
+    this.mkdir('css');
+    this.mkdir('js');
+    this.mkdir('img');
 
-  this.template('Gruntfile.js', 'Gruntfile.js');
-  this.template('index.html', 'index.html');
-  this.template('scss/main.scss', 'scss/main.scss');
-  this.template('scss/_modules.scss', 'scss/_modules.scss');
-  this.template('scss/_theme.scss', 'scss/_theme.scss');
+    this.template('Gruntfile.js', 'Gruntfile.js');
+    this.template('index.html', 'index.html');
+    this.template('scss/main.scss', 'scss/main.scss');
+    this.template('scss/_modules.scss', 'scss/_modules.scss');
+    this.template('scss/_theme.scss', 'scss/_theme.scss');
+    this.template('js/main.js', 'js/main.js');
 
-  this.copy('_package.json', 'package.json');
-  this.copy('_bower.json', 'bower.json');
+    this.copy('_package.json', 'package.json');
+    this.copy('_bower.json', 'bower.json');
 };
 
 FkGenerator.prototype.projectfiles = function projectfiles() {
-  this.copy('editorconfig', '.editorconfig');
-  this.copy('bowerrc', '.bowerrc');
-  this.copy('gitignore', '.gitignore');
+    this.copy('editorconfig', '.editorconfig');
+    this.copy('bowerrc', '.bowerrc');
+    this.copy('gitignore', '.gitignore');
 };
